d2618c9c8b1ba771dd86dfc58d41703bd4733f93
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-10-02 22:37:00 +0700

    add vue lazy load

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import router from "./router";
 
 import "./assets/css/base.css";
 import "./assets/css/style.css";
+import VueLazyload from "vue-lazyload";
+
+import loadImage from './assets/images/loader.gif'
+import errorImage from './assets/images/default.png'
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueLazyload, {
+    preLoad: 1.3,
+    error: errorImage,
+    loading: loadImage,
+    attempt: 1
+})
 
 app.mount("#app");
