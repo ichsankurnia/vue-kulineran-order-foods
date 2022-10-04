73b31b6068730e933939dbcb9dce025dc50028a8
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-10-04 20:26:00 +0700

    add darkmode

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -12,7 +12,7 @@
         <div class="imgBox w-full cursor-pointer" @click="() => this.$router.push('/foods/' + product.id)">
             <img v-lazy="product.gambar" alt="product.kode" class="rounded-t-2xl w-full h-52 md:h-[16.5rem] object-cover block" />
         </div>
-        <div class="border border-t-0 rounded-b-2xl py-5 px-6 -mt-2">
+        <div class="border-0 dark:border border-t-0 dark:border-gray rounded-b-2xl py-5 px-6 -mt-2 shadow-2xl dark:shadow-none">
             <h3 class="text-xl mb-1 font-medium">{{product.nama}}</h3>
             <p>Harga : Rp{{product.harga}}</p>
             <RouterLink :to="'/foods/' + product.id" class="bg-green text-white w-28 py-2 rounded-lg mt-3 hover-opacity flex justify-center items-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -7,6 +8,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        dark: '#181818',
+        darksoft: '#383838',
+        light: '#f5f5f5',
         green: '#00BD7E',
         primary: '#4EB883',
         gray: '#EBEBEBA3'
