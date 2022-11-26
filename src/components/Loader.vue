a7ef8dfb5f6e098c11df8485eee4f23a122a4815
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-11-27 01:09:18 +0700

    add loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import '../assets/css/coffee.css'
+</script>
+
+<template>
+    <div class="modal-form">
+        <div class="modal-form-outside"></div>
+
+        <div class="coffee">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</template>
